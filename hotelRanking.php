5d380a70a64d2d4d3f11323ccb847b6f9bbc4a0c
nouyeej <100678168+nouyeej@users.noreply.github.com>
2023-11-14 18:45:56 +0900

    Update and rename hotel_rating.php to hotelRanking.php
    Update and rename hotel_rating.php to hotelRanking.php

--- hotelRanking.php
+++ hotelRanking.php
@@ -50,7 +50,7 @@ $result = $conn->query($sql);
 <body>
 
     <div>
-        <a href="./index.php" class="icon">🏠</a>
+        <a href="./ranking.php" class="icon">🏠</a>
         <h1>Hotel Ranking</h1>
     </div>
     <table>
