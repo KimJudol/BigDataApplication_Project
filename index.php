6b39d9c71c01b9a462adc98aaa081df2ae53c68c
nouyeej <100678168+nouyeej@users.noreply.github.com>
2023-11-14 13:37:38 +0900

    ranking파일 업로드
    ranking, city/restaurant/hotel ranking 업로드

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rating</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            text-align: center;
+        }
+        .icon {
+            font-size: 24px;
+            cursor: pointer;
+        }
+        .title {
+            margin-top: 20px;
+        }
+        .buttons {
+            display: flex;
+            justify-content: space-around;
+            margin-top: 20px;
+        }
+        button {
+            font-size: 18px;
+            padding: 10px 20px;
+        }
+    </style>
+</head>
+<body>
+
+    <div>
+        <a href="#" class="icon">🏠</a>
+        <h1 class="title">Rating</h1>
+    </div>
+
+    <div class="buttons">
+        <button onclick="navigate('city')">City</button>
+        <button onclick="navigate('restaurant')">Restaurant</button>
+        <button onclick="navigate('hotel')">Hotel</button>
+    </div>
+
+    <script>
+        function navigate(category) {
+            if (category === 'city') {
+                window.location.href = 'city_rating.php';
+            }
+            else if (category === 'restaurant') {
+                window.location.href = 'restaurant_rating.php';
+            }
+            else if (category === 'hotel') {
+                window.location.href = 'hotel_rating.php';
+            }
+            else {
+                // 다른 카테고리의 동작을 추가할 수 있습니다.
+                alert('이 버튼은 ' + category + '를 나타냅니다.');
+            }
+        }
+    </script>
+
+</body>
+</html>
