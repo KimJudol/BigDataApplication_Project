a1f43645ed543941e02a20ca2cabbbaee02ed615
nouyeej <100678168+nouyeej@users.noreply.github.com>
2023-11-18 20:06:49 +0900

    Update searchWeather.php
    db 연결 , 삽입

--- searchWeather.php
+++ searchWeather.php
@@ -5,7 +5,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 // MySQL 데이터베이스에 연결
-$mysqli = mysqli_connect("localhost", "team18" "team18", "team18");
+$mysqli = mysqli_connect("localhost", "team18","team18", "team18");
 
 // 연결 오류 확인
 if ($mysqli->connect_error) {
