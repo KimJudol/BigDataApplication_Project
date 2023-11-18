cb16db30edb01b1050f5836907f573ffd4b2f502
nouyeej <100678168+nouyeej@users.noreply.github.com>
2023-11-18 21:09:09 +0900

    Update cityRanking.php
    delete style, add back button

--- cityRanking.php
+++ cityRanking.php
@@ -22,24 +22,6 @@ $result = $mysqli->query($sql);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>City Ranking</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            text-align: center;
-        }
-        table {
-            width: 50%;
-            margin: 20px auto;
-            border-collapse: collapse;
-        }
-        th, td {
-            padding: 10px;
-            border: 1px solid #ddd;
-        }
-        th {
-            background-color: #f2f2f2;
-        }
-    </style>
 </head>
 <body>
 
@@ -72,6 +54,8 @@ $result = $mysqli->query($sql);
     // 데이터베이스 연결 종료
     $mysqli->close();
     ?>
-
+<br><a href="/team18/ranking.html" target="_blank">
+        <button>Back</button>
+    </a><br>
 </body>
 </html>
