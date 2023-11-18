d8f62a0b4fe1dd27f7518f519aeb9c160a7a9186
KimJudol <wskje0106@ewhain.net>
2023-11-18 18:55:29 +0900

    searchHotel modified

--- searchHotel.php
+++ searchHotel.php
@@ -1,97 +1,77 @@
-<?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+    <?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
 
-// MySQL 데이터베이스에 연결
-$mysqli = mysqli_connect("localhost", "team18", "team18", "team18");
+    // MySQL 데이터베이스에 연결
+    $mysqli = mysqli_connect("localhost", "team18", "team18", "team18");
 
-// 연결 오류 확인
-if ($mysqli->connect_error) {
-    die("Connection failed: " . $mysqli->connect_error);
-}
+    // 연결 오류 확인
+    if ($mysqli->connect_error) {
+        die("Connection failed: " . $mysqli->connect_error);
+    }
 
-// 검색어 처리
-$search = isset($_GET['search']) ? $_GET['search'] : '';
-$search = mysqli_real_escape_string($mysqli, $search);
+    $city_name = $_POST["city_name"];
+    echo "<h1>".$city_name." Hotel List</h1>";
 
-// 검색 결과를 가져오는 쿼리
-$sql = "SELECT hotel.hotel_id, hotel.hotel_name, hotel.price, hotel.accomodation_number, hotel_discount.hotel_discount_Id, hotel_discount.hotel_discount_avaliable, hotel_discount.hotel_discount_rate, hotel_discount.hotel_discount_period
-        FROM hotel
-        LEFT JOIN hotel_discount ON hotel.hotel_id = hotel_discount.hotel_id
-        WHERE hotel.hotel_name LIKE '%$search%'
-        ORDER BY hotel.hotel_name";
+    // SQL 쿼리 실행
+    $sql = "
+        SELECT 
+            h.hotel_id, 
+            h.hotel_name, 
+            h.price, 
+            h.accomodation_number, 
+            hd.hotel_discount_Id, 
+            hd.hotel_discount_available, 
+            hd.hotel_discount_rate, 
+            hd.hotel_discount_period
+        FROM 
+            hotel h
+        JOIN 
+            city c ON h.city_id = c.city_id
+        JOIN 
+            hotel_discount hd ON h.hotel_id = hd.hotel_id
+        WHERE 
+            c.city_name = '$city_name';
+    ";
 
-$res = mysqli_query($mysqli, $sql);
+    $result = $mysqli->query($sql);
 
-?>
+    // HTML 테이블 시작
+    echo "<table border='1'>";
+    echo "<tr>
+            <th>Hotel ID</th>
+            <th>Hotel Name</th>
+            <th>Price</th>
+            <th>Accommodation Number</th>
+            <th>Discount ID</th>
+            <th>Discount Available</th>
+            <th>Discount Rate</th>
+            <th>Discount Period</th>
+        </tr>";
 
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Search Hotel</title>
-    <style>
-        body {
-            font-family: Consolas, monospace;
-            font-size: 12px;
+    // 결과 출력
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            // 행 추가
+            echo "<tr>
+                    <td>" . $row["hotel_id"] . "</td>
+                    <td>" . $row["hotel_name"] . "</td>
+                    <td>" . $row["price"] . "</td>
+                    <td>" . $row["accomodation_number"] . "</td>
+                    <td>" . $row["hotel_discount_Id"] . "</td>
+                    <td>" . $row["hotel_discount_available"] . "</td>
+                    <td>" . $row["hotel_discount_rate"] . "</td>
+                    <td>" . $row["hotel_discount_period"] . "</td>
+                </tr>";
         }
+    } else {
+        echo "<tr><td colspan='8'>No results found.</td></tr>";
+    }
 
-        table {
-            width: 100%;
-        }
+    // HTML 테이블 종료
+    echo "</table>";
 
-        th,
-        td {
-            padding: 10px;
-            border-bottom: 1px solid #dadada;
-        }
-    </style>
-</head>
-
-<body>
-    <div class="head">검색결과 | <?= $search ?></div>
-
-    <table style="width:1000px;" class="middle">
-        <thead>
-            <tr align="center">
-                <th width="70">Hotel ID</th>
-                <th width="300">Hotel Name</th>
-                <th width="120">Price</th>
-                <th width="120">Capacity</th>
-                <th width="120">Discount Status</th>
-                <th width="120">Discount Amount</th>
-                <th width="120">Validity Period</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-            while ($row = mysqli_fetch_assoc($res)) {
-            ?>
-                <tr align="center">
-                    <td><?php echo $row['hotel_id']; ?></td>
-                    <td><?php echo $row['hotel_name']; ?></td>
-                    <td><?php echo $row['price']; ?></td>
-                    <td><?php echo $row['accomodation_number']; ?></td>
-                    <td><?php echo $row['hotel_discount_avaliable']; ?></td>
-                    <td><?php echo $row['hotel_discount_rate']; ?></td>
-                    <td><?php echo $row['hotel_discount_period']; ?></td>
-                </tr>
-            <?php
-            }
-            ?>
-        </tbody>
-    </table>
-
-    <div class="search">
-        <form method="get" action="searchHotel.php">
-            <label for="search_box">Search:</label>
-            <input type="text" id="search_box" name="search" placeholder="Enter hotel name" value="<?= $search ?>">
-            <input type="submit" value="Search">
-        </form>
-    </div>
-</body>
-
-</html>
+    // MySQL 연결 종료
+    $mysqli->close();
+    ?>
