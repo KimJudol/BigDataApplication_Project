3621d346e2e4a12b2639d437e9106285df89c278
jiwon0723 <124261216+jiwon0723@users.noreply.github.com>
2023-11-18 22:06:03 +0900

    logout.php

--- logout.php
+++ logout.php
@@ -1,9 +1,11 @@
 <?php
+// 세션을 시작
 session_start();
 
 // 데이터베이스 연결
 $mysqli = mysqli_connect("localhost", "team18", "team18", "team18");
 
+// 데이터베이스 연결 오류를 확인
 if (!$mysqli) {
     die("데이터베이스 연결 실패: " . mysqli_connect_error());
 }
@@ -12,8 +14,6 @@ if (!$mysqli) {
 session_destroy();
 
 // 로그인 페이지로 리디렉션
-echo '<script type="text/javascript">
-           window.location = "login.html"
-      </script>';
+header("Location: login.html");
 exit();
 ?>
