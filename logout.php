2b36e17fe568e68db08f74c48c2a24a1e2d17130
nouyeej <100678168+nouyeej@users.noreply.github.com>
2023-11-18 22:02:37 +0900

    Update logout.php
    login으로 리디렉션 수정

--- logout.php
+++ logout.php
@@ -1,16 +1,19 @@
 <?php
+session_start();
+
 // 데이터베이스 연결
 $mysqli = mysqli_connect("localhost", "team18", "team18", "team18");
 
-//로그아웃버튼을 눌렀는지 확인
-if (!mysqli_select_db($mysqli, $databaseName)) {
-    die("데이터베이스 선택 실패: " . mysqli_error($mysqli));
+if (!$mysqli) {
+    die("데이터베이스 연결 실패: " . mysqli_connect_error());
 }
 
 // 세션을 파기하여 사용자 로그아웃
 session_destroy();
 
 // 로그인 페이지로 리디렉션
-header("Location: login.php");
+echo '<script type="text/javascript">
+           window.location = "login.html"
+      </script>';
 exit();
 ?>
